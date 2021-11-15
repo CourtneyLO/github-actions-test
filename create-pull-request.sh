83b84223828f3f0eb69f5a9d46833478ae161007
Courtney Osborn <courtney.osborn@bcgdv.com>
2021-11-15 15:29:57 +0000

    Update create-pull-request.sh

--- create-pull-request.sh
+++ create-pull-request.sh
@@ -3,13 +3,12 @@
 TOKEN=$1
 GITHUB_REF=$2
 
-echo $TOKEN
 echo $TOKEN > token.txt
 echo "hello"
-echo "$GITHUB_REF"
+echo $GITHUB_REF
 echo "hello"
 
 gh auth login --hostname GitHub.com --with-token < token.txt;
 gh pr view
-gh pr create --base production --head main --title "Release to Production ${GITHUB_REF}" --body "This work has been tested"
+gh pr create --base production --head main --title "Release to Production $GITHUB_REF" --body "This work has been tested"
 gh workflow run production.yml -F confirmation=YES
