debb352780ade5bf5dd9b886aaca84679a8b5140
Osborn Courtney <courtney.osborn@bcgdv.com>
2021-11-15 14:03:35 +0000

    test-30.0

--- create-pull-request.sh
+++ create-pull-request.sh
@@ -5,6 +5,6 @@ TOKEN=$1
 echo $TOKEN > token.txt
 
 gh auth login --hostname GitHub.com --with-token < token.txt;
-gh pr view
-gh pr create --base production --head main --title "Release to Production" --body "This work has been tested"
+echo gh pr view > pr.txt
+gh pr create --base production --head main --title "Release to Production" --body-file pr.txt
 gh workflow run production.yml -F confirmation=YES
